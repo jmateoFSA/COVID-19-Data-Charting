06112fa4d344d481bcd9f392832dc9425ee7a7c9
Juan Mateo <throwaway4classjmateo@gmail.com>
2022-01-09 20:57:40 -0500

    added second chart with global covid data

--- jm-covid-data-chart/src/App.js
+++ jm-covid-data-chart/src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Bar, Doughnut } from 'react-chartjs-2'
+import { Bar, PolarArea } from 'react-chartjs-2'
 import Chart from 'chart.js/auto'
 import { useDispatch, useSelector } from "react-redux";
 import { getStatesData } from './actions/statesAction';
@@ -13,8 +13,13 @@ function App() {
   const globalData = useSelector(state => state.globalData)
   const { loading, error, stateNames, stateCases } = usaData
   const { loading2, error2, countryNames, countryCases } = globalData
+
   useEffect(() => {
     dispatch(getStatesData())
+  }, [dispatch])
+
+  useEffect(() => {
+    dispatch(getGlobalData())
   }, [dispatch])
 
   const data = {
@@ -33,19 +38,20 @@ function App() {
 }
 
   const data2 = {
-    labels: ["One", "Two", "Three"],
+    labels: countryNames,
     datasets: [{
-      label: 'Global COVID-19 Cases',
-      data: [100, 200, 300],
-    backgroundColor: [
+        label: 'Global COVID-19 Cases',
+        data: countryCases,
+        backgroundColor: [
         'rgba(255, 99, 132, 0.4)',
-    ],
-    borderColor: [
+        ],
+        borderColor: [
         'rgba(0, 0, 0, 0.5)',
-    ],
-    hoverOffset: 4
+        ],
+        hoverOffset: 4
     }]
   }
+
   return (
     <div className="App">
       <div className="Chart1">
@@ -53,8 +59,11 @@ function App() {
         {error && <p>An Error Occurred...</p>}
         <Bar data={data} />
       </div>
+      <br />
       <div className="Chart2">
-        <Doughnut data={data2} />
+        {loading2 && <p>Loading Data...</p>}
+        {error2 && <p>An Error Occurred...</p>}
+        <PolarArea data={data2} />
       </div>
     </div>
   );
